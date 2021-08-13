00361663a37755546f95c6701a4bd9b9cdd8d811
tnorthern <traynorthern@yahoo.com>
2021-08-13 13:08:32 -0500

    Add hammer effect on click

--- components/misc/Mole.vue
+++ components/misc/Mole.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="w-4/5 my-20 md:my-24 md:mx-8 md:w-1/3 lg:w-1/5">
     <div class="w-full h-3 bg-black relative group block">
-      <img class="absolue w-full h-40 inset-0 z-40" src="/hammer.png" alt="" />
+      <img
+        class="absolute w-full h-40 inset-x-0 -top-42 duration-150"
+        :class="didClick ? 'opacity-100 z-10' : 'opacity-0 z-0'"
+        src="/hammer.png"
+        alt=""
+      />
       <img
         :class="isShown ? 'max-h-40' : 'max-h-0'"
         class="
@@ -29,6 +34,22 @@ export default {
     isShown: false,
     didClick: false,
   }),
+  watch: {
+    didClick(value) {
+      if (value) {
+        const vm = this
+        setTimeout(() => {
+          vm.didClick = false
+          vm.isShown = false
+          vm.stopMole()
+          vm.startMole()
+        }, 400)
+      }
+    },
+  },
+  beforeDestroy() {
+    clearInterval(this.intervalHandler)
+  },
   mounted() {
     this.startMole()
   },
@@ -43,10 +64,21 @@ export default {
       clearInterval(this.intervalHandler)
     },
     resetMole() {
-      this.isShown = false
-      this.stopMole()
-      this.startMole()
+      this.didClick = true
     },
   },
 }
 </script>
+
+<style scoped>
+.appear-in {
+  animation: 0.1s appear ease-in;
+  /* margin: auto; */
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
